0bc5c9d99893d02f4007d19464dfab3f119ad452
Filip Dabkowski <filip.dabkowski@gmail.com>
2021-01-06 16:01:35 +0100

    random file coding pm done

--- file.py
+++ file.py
@@ -1,34 +1,6 @@
 import random
 import hashlib
 
-some_string = 'Hello World'
-my_bytes = bytes(some_string, 'utf-8')
-print('Word: ', my_bytes)
-
-print('Word in bites: ', list(bytes(my_bytes)))
-
-my_list = list(bytes(my_bytes))
-my_list_validation = my_list[:]
-
-randoms = []
-
-
-def get_r(index):
-    if index + 1 > len(randoms):
-        if index > 3:
-            randoms.append(random.randint(1, 255))
-        else:
-            randoms.append(0)
-    return randoms[index]
-
-
-my_list = [my_list[i] + get_r(i) if my_list[i] + get_r(i) <= 255 else my_list[i] + get_r(i) - 256 for i in range(len(my_list))]
-
-x = 7
-my_list_validation = [i + x if i + x <= 255 else i + x - 256 for i in my_list_validation]
-
-print('Word in random every bite: ', my_list)
-print('Word in not random move: ', my_list_validation)
 
 # random.seed('hej')
 #
@@ -40,5 +12,95 @@ print('Word in not random move: ', my_list_validation)
 # my_list = list(bytes(hashed))
 # print(my_list)
 
-def random_coding():
 
+def random_coding():
+    with open('test/todo-code.txt', 'rb') as file:
+        data = file.read()
+        byte_list = list(bytes(data))
+
+        my_seed = byte_list[0] * 4096 + byte_list[1] * 256 + byte_list[2] * 16 + byte_list[3]
+
+        random.seed(my_seed)
+
+        randoms = []
+
+        def get_r(index):
+            if index + 1 > len(randoms):
+                if index > 3:
+                    randoms.append(random.randint(1, 255))
+                else:
+                    randoms.append(0)
+            return randoms[index]
+
+        byte_list = [byte_list[i] + get_r(i) if byte_list[i] + get_r(i) <= 255 else byte_list[i] + get_r(i) - 256 for i
+                     in range(len(byte_list))]
+
+        new_bytes = bytes(byte_list)
+
+        with open('test/coded.txt', 'wb') as copy:
+            copy.write(new_bytes)
+
+
+def random_decoding():
+    with open('test/todo-decode.txt', 'rb') as file:
+        data = file.read()
+        byte_list = list(bytes(data))
+
+        my_seed = byte_list[0] * 4096 + byte_list[1] * 256 + byte_list[2] * 16 + byte_list[3]
+
+        random.seed(my_seed)
+
+        randoms = []
+
+        def get_r(index):
+            if index + 1 > len(randoms):
+                if index > 3:
+                    randoms.append(random.randint(1, 255))
+                else:
+                    randoms.append(0)
+            return randoms[index]
+
+        byte_list = [byte_list[i] - get_r(i) if byte_list[i] - get_r(i) >= 0 else byte_list[i] - get_r(i) + 256 for i in
+                     range(len(byte_list))]
+
+        new_bytes = bytes(byte_list)
+
+        with open('test/decoded.txt', 'wb') as copy:
+            copy.write(new_bytes)
+
+
+def normal_coding():
+    with open('codeTest.txt', 'rb') as file:
+        data = file.read()
+        byte_list = list(bytes(data))
+
+        r = 7
+        byte_list = [i + r if i + r <= 255 else i + r - 256 for i in byte_list]
+
+        new_bytes = bytes(byte_list)
+
+        with open('decodeTest.txt', 'wb') as copy:
+            copy.write(new_bytes)
+
+
+def normal_decoding():
+    with open('decodeTest.txt', 'rb') as file:
+        data = file.read()
+        byte_list = list(bytes(data))
+
+        r = 7
+        byte_list = [i - r if i - r >= 0 else i - r + 256 for i in byte_list]
+
+        new_bytes = bytes(byte_list)
+
+        with open('codeTest.txt', 'wb') as copy:
+            copy.write(new_bytes)
+
+
+# normal_coding()
+#
+# normal_decoding()
+
+# random_coding()
+#
+# random_decoding()
